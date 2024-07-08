d854ebfc0f07dbbf7b2fb451a7f76762baddd11f
Amina AbdelAzeez <aminaabdelazeez99@gmail.com>
2024-07-08 13:25:08 +0300

    my changes

--- js/script.js
+++ js/script.js
@@ -100,3 +100,35 @@ window.onscroll = () => {
     })
 }
 // end smooth scrolling animation
+
+
+// for multi-language 
+
+import translations from "./translations.js";
+
+const languageSelector =  document.querySelector("select");
+languageSelector.addEventListener("change", (event) => {
+    setLanguage(event.target.value);
+    localStorage.setItem("lang", event.target.value)
+})
+
+document.addEventListener("DOMContentLoaded" , () => {
+    const language = localStorage.getItem("lang");
+    setLanguage(language);
+    // setLanguage(localStorage.getItem("lang"));
+})
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll("[data-i18]")
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute("data-i18");
+        element.textContent = translations[language][translationKey]
+    });
+
+    document.dir = language === "ar" ? "rtl" : "ltr";
+
+    // if(language === "ar") {
+    //     document.dir = "rtl"
+    // } else {
+    //     document.dir = "ltr"
+    // }
+}
